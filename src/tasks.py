6d0fc96cc3b9e3c3d65f6e7396b6c171a978fbfa
Toni Crespi <crespi.toni@gmail.com>
2024-03-24 13:19:21 +0100

    fix: ran pre-commit hooks on all files

--- src/tasks.py
+++ src/tasks.py
@@ -1,10 +1,15 @@
-_tasks = []
+from typing import List
+
+_tasks: List = []
+
 
 def get_tasks():
     return _tasks
 
+
 def add_task(task):
     _tasks.append(task)
+
 
 def remove_task(task_id):
     _tasks.pop(task_id)
--- tests/integration/test_api.py
+++ tests/integration/test_api.py
@@ -1,4 +1,5 @@
 import unittest
+
 from src.app import app
 
 
@@ -8,16 +9,17 @@ class TestApp(unittest.TestCase):
         self.app.testing = True
 
     def test_get_tasks(self):
-        response = self.app.get('/tasks')
+        response = self.app.get("/tasks")
         self.assertEqual(response.status_code, 200)
 
     def test_add_and_remove_task(self):
-        post_response = self.app.post('/tasks', json={'task': 'New Task'})
+        post_response = self.app.post("/tasks", json={"task": "New Task"})
         self.assertEqual(post_response.status_code, 201)
-        delete_response = self.app.delete('/tasks/0')
+        delete_response = self.app.delete("/tasks/0")
         self.assertEqual(delete_response.status_code, 204)
-        get_response = self.app.get('/tasks')
+        get_response = self.app.get("/tasks")
         self.assertEqual(get_response.json, [])
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
